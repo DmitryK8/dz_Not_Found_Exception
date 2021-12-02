f772eeb03e055cf452a628ddc0c68d2e9dff9107
Dmitry Kravchenko <d.k.12.1972@gmail.com>
2021-12-02 14:46:58 +0500

    переделал репо

--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -1,82 +1,40 @@
 package ru.netology.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
+import ru.netology.domain.NotFoundException;
 import ru.netology.domain.Product;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ProductRepositoryTest {
     private ProductRepository repository = new ProductRepository();
-    private Book goodBook = new Book();
+    private Book goodBook = new Book(1, "GoodBook", 1000, "Толстой");
     private Book goodBook8 = new Book(11, "Good Book8", 1107, "Дмитрий Кравченко");
     private Book goodBook17 = new Book(117, "Good Book17 ", 2060, "Вася Пупкин");
-    private String id;
 
-    @Test
-
-    public void shouldSave1Book() {
+    @BeforeEach
+    public void shouldSetUp() {
         repository.save(goodBook);
-
-        Product[] expected = new Product[]{goodBook};
-        Product[] actual = repository.findAll();
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSave8Book() {
-        repository.save(goodBook8);
-
-        Product[] expected = new Product[]{goodBook8};
-        Product[] actual = repository.findAll();
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSave2Books() {
         repository.save(goodBook8);
         repository.save(goodBook17);
 
-        Product[] expected = new Product[]{goodBook8, goodBook17};
-        Product[] actual = repository.findAll();
-        assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldSearchId() {
-        int idSearch = 1107;
-
-        repository.save(goodBook8);
-        repository.save(goodBook17);
-        try {
-        repository.searchId(idSearch);
-
-        Product[] expected = new Product[]{goodBook8};
-
-            Product[] actual = repository.findAll();
-            assertArrayEquals(expected, actual);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Element with id: " + id + " not found");
-        } catch (Exception e) {
-        }
+    public void shouldRemoveById() {
+        Product expected = goodBook8;
+        Product actual = repository.findById(11);
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldSearchId117() {
-        int idSearch = 1000;
-
-        repository.save(goodBook8);
-        repository.save(goodBook17);
-        try {
-            repository.searchId(idSearch);
-
-            Product[] expected = new Product[]{goodBook8};
-
-            Product[] actual = repository.findAll();
-            assertArrayEquals(expected, actual);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("ОШИБКА: такой id не существует!!!");
-        } catch (Exception e) {
-        }
+    public void shouldFindByIdNot() {
+        Product expected = null;
+        assertThrows(NotFoundException.class, () -> repository.removeById(1000));
     }
+
+
 }
